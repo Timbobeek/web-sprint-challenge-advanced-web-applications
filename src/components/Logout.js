30edb951eba03dd19953463c12bd00de563cf589
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-28 15:12:22 -0600

    logout works

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 
-const Logout = () => {        
+const Logout = () => { 
+    const {push} = useHistory();
+    useEffect(()=>{
+        const token = localStorage.getItem('token');
+        console.log(token);
+        axios.post('http://localhost:5000/api/logout','*',{
+            headers: {
+                authorization: token
+            }
+        })
+        .then(res => {
+            console.log(res);
+            localStorage.removeItem('token');
+            push('/login');
+
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    },[])       
     return(<div></div>);
 }
 
