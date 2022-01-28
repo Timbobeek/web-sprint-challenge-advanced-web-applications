cc0716d2a19a3403694cbda21afd45397bc5ccc0
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-27 20:42:08 -0600

    basic routing done

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
+import View from './View';
+import Logout from './Logout';
 
 const App = () => {
   return (
@@ -12,9 +14,24 @@ const App = () => {
       <BloomHeader/>
       <Header/>
       <RouteContainer>
+        
         <Route exact path="/">
           <Login/>
-        </Route>          
+        </Route>
+        
+        <Route path="/login">
+          <Login/>
+        </Route>
+        
+        <Route path="/view">
+          <View/>
+        </Route>
+        
+        <Route path="/logout">
+          <Logout/>
+        </Route>
+
+
       </RouteContainer>
     </AppContainer>
   )
